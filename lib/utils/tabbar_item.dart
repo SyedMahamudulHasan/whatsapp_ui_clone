0a50900e140536a972bdafcd56650d4271da4709
omedia23 <syedhasan.cse@gmail.com>
2022-09-01 10:55:20 +0600

    create tabBarItem

---
+++ lib/utils/tabbar_item.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import '../constants/app_color.dart';
+
+class TabBarItem {
+  static final msg_notification = 1;
+  static final tabList = [
+    const Tab(
+      icon: Icon(Icons.camera_alt_rounded),
+    ),
+    Tab(
+      //text: "CHATS",
+      child: Row(
+        children: [
+          const Flexible(
+            child: Padding(
+              padding: EdgeInsets.only(right: 5.0),
+              child: Text('CHATS'),
+            ),
+          ),
+          msg_notification == 0
+              ? Container()
+              : Container(
+                  padding: const EdgeInsets.all(6),
+                  decoration: const BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Colors.white,
+                  ),
+                  child: Text(
+                    msg_notification.toString(),
+                    style: TextStyle(
+                      color: AppColors.appbarColor,
+                    ),
+                  ),
+                )
+        ],
+      ),
+    ),
+    const Tab(
+      text: "STATUS",
+    ),
+    const Tab(
+      text: "CALLS",
+    ),
+  ];
+}
--- lib/view/home_screen.dart
+++ lib/view/home_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:whatsapp_ui_clone/constants/app_color.dart';
 
+import '../utils/tabbar_item.dart';
+
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
@@ -21,46 +23,6 @@ class _HomeScreenState extends State<HomeScreen>
 
   @override
   Widget build(BuildContext context) {
-    final msg_notification = 1;
-    final tabList = [
-      const Tab(
-        icon: Icon(Icons.camera_alt_rounded),
-      ),
-      Tab(
-        //text: "CHATS",
-        child: Row(
-          children: [
-            const Flexible(
-              child: Padding(
-                padding: EdgeInsets.only(right: 5.0),
-                child: Text('CHATS'),
-              ),
-            ),
-            msg_notification == 0
-                ? Container()
-                : Container(
-                    padding: const EdgeInsets.all(6),
-                    decoration: const BoxDecoration(
-                      shape: BoxShape.circle,
-                      color: Colors.white,
-                    ),
-                    child: Text(
-                      msg_notification.toString(),
-                      style: TextStyle(
-                        color: AppColors.appbarColor,
-                      ),
-                    ),
-                  )
-          ],
-        ),
-      ),
-      const Tab(
-        text: "STATUS",
-      ),
-      const Tab(
-        text: "CALLS",
-      ),
-    ];
     return Scaffold(
       appBar: AppBar(
         backgroundColor: AppColors.appbarColor,
@@ -96,7 +58,7 @@ class _HomeScreenState extends State<HomeScreen>
           indicatorColor: Colors.white,
           indicatorWeight: 3.0,
           //labelPadding: EdgeInsets.all(8),
-          tabs: tabList.map((tab) => tab).toList(),
+          tabs: TabBarItem.tabList.map((tab) => tab).toList(),
         ),
       ),
       body: TabBarView(
